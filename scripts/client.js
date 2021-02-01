2aee38aab2cc15769c6a07102ead5aea643822ea
George Papagapitos <george.g.papagapitos@gmail.com>
2021-02-01 16:31:29 -0600

    random name generated

--- scripts/client.js
+++ scripts/client.js
@@ -3,7 +3,10 @@ console.log('Here are all the available people:', people);
 $(document).ready(onReady);
 
 function onReady() {
-  $('body').prepend(`<h2>Click on: </h2>`);
+  let randomName = people[Math.floor(Math.random() * people.length - 1)].name;
+  $('body').prepend(
+    `<h2>Click on: <span class="random-name">${randomName}</span></h2>`
+  );
   for (person of people) {
     $('body').append(
       `<div data-name=${person.name}><img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}"></div>`
